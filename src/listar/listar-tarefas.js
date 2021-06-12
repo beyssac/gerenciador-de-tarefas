82aea227d918e44553769ecfdc331c000762b48e
Pedro Henrique <pedrohenrique.ufrj@gmail.com>
2021-06-12 20:46:03 -0300

    Adicionando useEffet para exibir tarefas

--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {A} from 'hookrouter';
+import {Table} from 'react-bootstrap';
+import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
+import {faPlus} from '@fortawesome/free-solid-svg-icons';
 
 function ListarTarefas(){
+
+    const [tarefas, setTarefas] = useState([]);
+    const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+    useEffect(() => {
+        function obterTarefas(){
+            const tarefasDb = localStorage['tarefas'];
+            let ListarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            setTarefas(ListarTarefas);
+            console.log(ListarTarefas);
+        }
+        if(carregarTarefas){
+            obterTarefas();
+            setCarregarTarefas(false);
+        }        
+    },[carregarTarefas]);
+
     return(
-        <A  href="/cadastrar"
-            className="btn btn-success btn-sm">
-            Nova Tarefa
-        </A>
+        <div className="text-center">
+            <h3>Tarefas a fazer</h3>
+            <Table striped bordered hover responsive data-testid="tabela">
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>
+                            <A href="/cadastrar"
+                               className="btn btn-success btn-sm"
+                               data-testid="btn-nova-tarefa">
+                                   <FontAwesomeIcon icon={faPlus}/>
+                                   &nbsp;
+                                   Nova Tarefa
+                            </A>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </Table>
+        </div>
     );
 }
 
