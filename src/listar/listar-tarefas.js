23572e4e1fc5cd69c57a7b4857b9f1ffb044dd87
Pedro Henrique <pedrohenrique.ufrj@gmail.com>
2021-06-20 18:42:31 -0300

    Adicionando paginação

--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -4,24 +4,34 @@ import {Table} from 'react-bootstrap';
 import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
 import ItensListarTarefas from './itens-lista-tarefas';
+import Paginacao from './paginacao';
 
 function ListarTarefas(){
 
+    const ITENS_POR_PAG = 3;
+
     const [tarefas, setTarefas] = useState([]);
     const [carregarTarefas, setCarregarTarefas] = useState(true);
+    const [totalItems, setTotalItems] = useState(0);
+    const [paginaAtual, setPaginaAtual] = useState(1);
 
     useEffect(() => {
         function obterTarefas(){
             const tarefasDb = localStorage['tarefas'];
-            let ListarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
-            setTarefas(ListarTarefas);
-            console.log(ListarTarefas);
+            let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            setTotalItems(listaTarefas.length);
+            setTarefas(listaTarefas.splice((paginaAtual -1 ) * ITENS_POR_PAG, ITENS_POR_PAG));            
         }
         if(carregarTarefas){
             obterTarefas();
             setCarregarTarefas(false);
         }        
-    },[carregarTarefas]);
+    },[carregarTarefas, paginaAtual]);
+
+    function handleMudarPagina(pagina){
+        setPaginaAtual(pagina);
+        setCarregarTarefas(true);
+    }
 
     return(
         <div className="text-center">
@@ -47,6 +57,11 @@ function ListarTarefas(){
                     recarregarTarefas={setCarregarTarefas}/>  
                 </tbody>
             </Table>
+            <Paginacao
+                totalItems={totalItems}
+                itemsPorPagina={ITENS_POR_PAG}
+                paginaAtual={paginaAtual}
+                mudarPagina={handleMudarPagina}/>
         </div>
     );
 }
