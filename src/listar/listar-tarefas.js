f2f4c7ed99068204bb9c17f0a801d78cefbb6ae1
Pedro Henrique <pedrohenrique.ufrj@gmail.com>
2021-06-21 13:45:23 -0300

    Adicionando ordenação

--- src/listar/listar-tarefas.js
+++ src/listar/listar-tarefas.js
@@ -5,6 +5,7 @@ import {FontAwesomeIcon}  from '@fortawesome/react-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
 import ItensListarTarefas from './itens-lista-tarefas';
 import Paginacao from './paginacao';
+import Ordenacao from './ordenacao';
 
 function ListarTarefas(){
 
@@ -14,11 +15,19 @@ function ListarTarefas(){
     const [carregarTarefas, setCarregarTarefas] = useState(true);
     const [totalItems, setTotalItems] = useState(0);
     const [paginaAtual, setPaginaAtual] = useState(1);
+    const [ordenarAsc, setOrdenarAsc] = useState(false);
+    const [ordenarDesc, setOrdenarDesc] = useState(false);
 
     useEffect(() => {
         function obterTarefas(){
             const tarefasDb = localStorage['tarefas'];
             let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+             //ordenar
+            if (ordenarAsc) {
+                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase()) ? 1 : -1);
+            } else if (ordenarDesc) {
+                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase()) ? 1 : -1);
+            }
             setTotalItems(listaTarefas.length);
             setTarefas(listaTarefas.splice((paginaAtual -1 ) * ITENS_POR_PAG, ITENS_POR_PAG));            
         }
@@ -26,12 +35,27 @@ function ListarTarefas(){
             obterTarefas();
             setCarregarTarefas(false);
         }        
-    },[carregarTarefas, paginaAtual]);
+    },[carregarTarefas, paginaAtual, ordenarAsc, ordenarDesc]);
 
     function handleMudarPagina(pagina){
         setPaginaAtual(pagina);
         setCarregarTarefas(true);
     }
+
+    function handleOrdenar(event) {
+        event.preventDefault();
+        if (!ordenarAsc && !ordenarDesc) {
+          setOrdenarAsc(true);
+          setOrdenarDesc(false);
+        } else if (ordenarAsc) {
+          setOrdenarAsc(false);
+          setOrdenarDesc(true);
+        } else {
+          setOrdenarAsc(false);
+          setOrdenarDesc(false);
+        }
+        setCarregarTarefas(true);
+      }
 
     return(
         <div className="text-center">
@@ -39,7 +63,15 @@ function ListarTarefas(){
             <Table striped bordered hover responsive data-testid="tabela">
                 <thead>
                     <tr>
-                        <th>Tarefa</th>
+                        <th>
+                            <a href="/" onClick={handleOrdenar}>
+                                Tarefa
+                                &nbsp;
+                                <Ordenacao
+                                    ordenarAsc={ordenarAsc}
+                                    ordenarDesc={ordenarDesc} />
+                            </a>
+                        </th>
                         <th>
                             <A href="/cadastrar"
                                className="btn btn-success btn-sm"
